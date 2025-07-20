e8603c73d52ba0dcbde7e9407fd95b93fd985567
Nghia892 <97269460+Nghia892@users.noreply.github.com>
2025-07-20 23:14:39 +0700

    Update ROLL.lua

--- ROLL.lua
+++ ROLL.lua
@@ -13,12 +13,12 @@ local StartRolls = false
 
 local function Roll()
     local args = {
-	    "ub_sun",
+	    "ub_bee",
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
     local args = {
-	    "ub_sun",
+	    "ub_bee",
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
