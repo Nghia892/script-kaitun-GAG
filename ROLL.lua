deb4cce2f9c4ed6066816499b0ccc4329666ece0
Nghia892 <97269460+Nghia892@users.noreply.github.com>
2025-08-16 22:02:23 +0700

    Update ROLL.lua

--- ROLL.lua
+++ ROLL.lua
@@ -1,5 +1,5 @@
-local SeedWaitRoll = 7200
-local SeedStopRoll = 2700
+local SeedWaitRoll = 9000
+local SeedStopRoll = 4500
 local _wait = task.wait
 
 repeat _wait() until game:IsLoaded()
@@ -12,14 +12,19 @@ local deleteRemote = ReplicatedStorage:WaitForChild("RemoteFunctions"):WaitForCh
 local StartRolls = false
 
 local function Roll()
+	local args = {
+	    "unit_buff_carrot"
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitWithSeeds"):InvokeServer(unpack(args))
+	task.wait(1)
     local args = {
-	    "ub_lucky_2",
+	    "ub_corrupted",
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
 	task.wait(0.5)
     local args = {
-	    "ub_lucky_2",
+	    "ub_corrupted",
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
@@ -35,7 +40,7 @@ local function RemoveUnit()
 		local itemId = unitData.ItemData and unitData.ItemData.ID
 		local rarity = require(game:GetService("Players").LocalPlayer.PlayerGui.LogicHolder.ClientLoader.SharedConfig.ItemData.Units.Configs:FindFirstChild(itemId))
 
-		if rarity.Rarity == "ra_godly" or rarity.Rarity == "ra_exclusive" or unitData.Equipped == true then
+		if rarity.Rarity == "ra_godly" or unitData.Equipped == true then
 			kept[itemId] = true
 			continue
 		end
@@ -79,7 +84,5 @@ end
 -- 	end
 -- 	_wait(5)
 -- end
-
-
 
 
